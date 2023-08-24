4d96bdd45a8c9e49fb2c8af1bfe3c66ecc3dccf8
CatWhoProgramming <kat.servetnik@gmail.com>
2023-08-24 22:25:18 +0200

    Create class Main in task3 In this class we used method substring of String class

---
+++ Task3/src/Task3/Main.java
@@ -0,0 +1,11 @@
+package Task3;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Необхідно створити рядок із текстом (текст взяти будь-який з інтернету).";
+        System.out.println(str.substring(0,str.length()/2));
+        System.out.println(str.substring(str.length()/2+1));
+    }
+
+
+}
